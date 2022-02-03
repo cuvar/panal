e48430bf6fd5c96f11d614840bb12737902fef33
cuvar <luca-mueller@gmx.net>
2022-02-03 19:48:17 +0100

    feat: color palette + fix tw bug nuxt config

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="flex justify-center items-center h-screen bg-teal-800">
-    <p class="font-bold text-3xl text-indigo-100">panal - by cuvar</p>
+  <div class="flex flex-col justify-center items-center h-screen bg-teal-800">
+    <p class="font-bold text-3xl text-pwhite-100">panal - by cuvar</p>
+    <p class="font-bold text-3xl text-plightblue-500">panal - by cuvar</p>
+    <p class="font-bold text-3xl text-pgreen-500">panal - by cuvar</p>
+    <p class="font-bold text-3xl text-pdarkblue-500">panal - by cuvar</p>
   </div>
 </template>
