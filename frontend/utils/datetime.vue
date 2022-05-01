5274f0156c6d6b570d81637e544c91afae877a59
cuvar <luca-mueller@gmx.net>
2022-05-01 18:13:42 +0200

    feat: current date and padding function

---
+++ frontend/utils/datetime.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { padNumber } from "./string.vue";
+
+function getCurrentDateHumanReadable(): string {
+  const date: Date = new Date();
+  const year: number = date.getFullYear();
+  //@ts-ignore
+  const month: string = padNumber(date.getMonth() + 1);
+  const day: string = padNumber(date.getDate());
+  return `${day}.${month}.${year}`;
+}
+
+export { getCurrentDateHumanReadable };
+</script>
