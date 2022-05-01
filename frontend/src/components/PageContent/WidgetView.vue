211f304a46473f26f46312793a5e8fd014ed8325
cuvar <luca-mueller@gmx.net>
2022-05-01 17:07:25 +0200

    rm: spring

--- frontend/src/components/PageContent/WidgetView.vue
+++ frontend/src/components/PageContent/WidgetView.vue
@@ -1,6 +1,38 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import WidgetContainer from "./WidgetContainer.vue";
+import type { IWidgetData } from "../../../utils/types/WidgetData.vue";
+
+interface IProps {
+  widgetData: IWidgetData[];
+}
+const props = defineProps<IProps>();
+
+if (!validateWidgetData(props.widgetData)) {
+  console.warn(`Invalid widget data: ${JSON.stringify(props.widgetData)}`);
+}
+
+function validateWidgetData(value: IWidgetData[]) {
+  // Todo: https://stackoverflow.com/questions/71103028/how-to-use-validator-in-vue-using-setup-script-typscript-and-the-composition-ap
+  // Todo: https://vuejs.org/api/sfc-spec.html#language-blocks
+  return value.every((widgetData: IWidgetData) => {
+    return (
+      0 < widgetData.rowSpan &&
+      0 < widgetData.colSpan &&
+      0 < widgetData.order &&
+      widgetData.title.length > 0
+    );
+  });
+}
+
+const rowClass: string = "auto-rows-min";
+const colClass: string =
+  "grid-cols-3 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-8";
+</script>
+
 <template>
   <!-- //TODO: max-w-screen-2xl sorgt für fehler in side margin -->
-  <div class="h-full grid gap-x-8 gap-y-8" :class="[getRowClass, getColClass]">
+  <div class="h-full grid gap-x-8 gap-y-8" :class="[rowClass, colClass]">
     <WidgetContainer
       v-for="(widget, index) in widgetData"
       :key="index"
@@ -11,40 +43,3 @@
     ></WidgetContainer>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import WidgetContainer from "./WidgetContainer.vue";
-import type { IWidgetData } from "../../../utils/types/WidgetData.vue";
-
-const WidgetView = defineComponent({
-  components: {
-    WidgetContainer,
-  },
-  props: {
-    widgetData: {
-      type: Array as () => IWidgetData[],
-      required: true,
-      validator(value: IWidgetData[]) {
-        return value.every((widgetData: IWidgetData) => {
-          return (
-            0 < widgetData.rowSpan &&
-            0 < widgetData.colSpan &&
-            0 < widgetData.order &&
-            widgetData.title.length > 0
-          );
-        });
-      },
-    },
-  },
-  computed: {
-    getRowClass() {
-      return "auto-rows-min";
-    },
-    getColClass() {
-      return "grid-cols-3 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-8";
-    },
-  },
-});
-export default WidgetView;
-</script>
