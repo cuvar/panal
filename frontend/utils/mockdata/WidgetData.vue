e782015e6953ed1bea50c535941f71c82af02ed7
cuvar <luca-mueller@gmx.net>
2022-05-18 18:35:41 +0200

    fix: adapt widget test data

--- frontend/utils/mockdata/WidgetData.vue
+++ frontend/utils/mockdata/WidgetData.vue
@@ -1,13 +1,16 @@
 <script lang="ts">
+import { getCurrentDateHumanReadable } from "../datetime.vue";
 import { IWidgetData } from "../types/WidgetData.vue";
-const AMOUNT_WIDGET: number = 60; //10x6
+const AMOUNT_WIDGET: number = 1; //10x6
 const mockWidgetData: IWidgetData[] = [...Array(AMOUNT_WIDGET).keys()].map(
   (i) => {
     return {
-      rowSpan: 1,
+      rowSpan: 3,
       colSpan: 1,
+      rowStart: 1,
+      colStart: 3,
       order: i + 1,
-      title: `Widget ${i + 1}`,
+      title: `${getCurrentDateHumanReadable()}`,
     };
   }
 );
