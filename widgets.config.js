71badd658584e1311aa20a84d5a5bc8970afac7d
cuvar <luca-mueller@gmx.net>
2022-06-03 23:00:12 +0200

    feat: added mock widget data

--- widgets.config.js
+++ widgets.config.js
@@ -5,22 +5,64 @@ export default [
   {
     id: "cjld2cyuq0000t3rmniod1foy",
     lg: {
-      rowStart: 2,
+      rowStart: 1,
       colStart: 1,
       rowSpan: 1,
       colSpan: 1,
     },
     md: {
-      rowStart: 2,
+      rowStart: 1,
       colStart: 1,
       rowSpan: 1,
       colSpan: 1,
     },
     sm: {
-      rowStart: 2,
+      rowStart: 1,
       colStart: 1,
       rowSpan: 1,
       colSpan: 1,
+    }
+  },
+  {
+    id: "cjld2cyuq0000t3rmniod1kqi",
+    lg: {
+      rowStart: 1,
+      colStart: 2,
+      rowSpan: 2,
+      colSpan: 2,
+    },
+    md: {
+      rowStart: 1,
+      colStart: 2,
+      rowSpan: 2,
+      colSpan: 2,
+    },
+    sm: {
+      rowStart: 1,
+      colStart: 2,
+      rowSpan: 2,
+      colSpan: 2,
+    }
+  },
+  {
+    id: "cjld2cyuq0000t3rmniod1kqi",
+    lg: {
+      rowStart: 3,
+      colStart: 1,
+      rowSpan: 1,
+      colSpan: 3,
+    },
+    md: {
+      rowStart: 3,
+      colStart: 1,
+      rowSpan: 1,
+      colSpan: 3,
+    },
+    sm: {
+      rowStart: 3,
+      colStart: 1,
+      rowSpan: 1,
+      colSpan: 3,
     }
   },
 ]
