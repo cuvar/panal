f36ec0f9d7d0a155923c63531004e5df043bb2a2
cuvar <luca-mueller@gmx.net>
2022-06-03 22:22:12 +0200

    fix: new testdata

--- widgets.config.js
+++ widgets.config.js
@@ -1,6 +1,9 @@
+// todo: optional xl param for config
+// todo: adapt grid col classes by panal.config  
+
 export default [
   {
-    id: "cuid()",
+    id: "cjld2cyuq0000t3rmniod1foy",
     lg: {
       rowStart: 2,
       colStart: 1,
@@ -20,25 +23,4 @@ export default [
       colSpan: 1,
     }
   },
-  {
-    id: "cuid()",
-    lg: {
-      rowStart: 2,
-      colStart: 1,
-      rowSpan: 1,
-      colSpan: 1,
-    },
-    md: {
-      rowStart: 2,
-      colStart: 1,
-      rowSpan: 1,
-      colSpan: 1,
-    },
-    sm: {
-      rowStart: 2,
-      colStart: 1,
-      rowSpan: 1,
-      colSpan: 1,
-    }
-  }
 ]
