58d99f849bafa7a60eb4b411d9e72c010ab37cf9
cuvar <luca-mueller@gmx.net>
2022-06-22 10:32:04 +0200

    feat: config.type ts type

--- utils/types/WidgetData.ts
+++ utils/types/WidgetData.ts
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 declare interface IWidgetPositionData {
   rowSpan: number;
@@ -6,12 +6,24 @@ declare interface IWidgetPositionData {
   rowStart: number;
   colStart: number;
 }
+
+declare type TType = 'datetime' | 'gcalendar';
+declare interface IWidgetConfig {
+  type: TType;
+}
 declare interface IWidgetData {
-  id: string;
   sm: IWidgetPositionData;
   md: IWidgetPositionData;
   lg: IWidgetPositionData;
+  config: IWidgetConfig;
   children: React.ReactNode;
 }
 
-export type { IWidgetData, IWidgetPositionData };
+// widget config types
+
+declare interface IDateTimeWidgetConfig extends IWidgetConfig {
+  time?: boolean;
+  date?: boolean;
+}
+
+export type { IWidgetData, IWidgetPositionData, IWidgetConfig, TType, IDateTimeWidgetConfig };
