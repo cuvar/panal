0c3a36ea2c6e410f6615e634c04a1beccdcb9bea
cuvar <luca-mueller@gmx.net>
2022-10-24 09:02:20 +0200

    fix: footer semantic tag

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,6 @@
 	import Link from './Link.svelte';
 </script>
 
-<div class="w-full bg-panal-700 py-5 px-5 flex justify-center">
+<footer class="w-full bg-panal-700 py-5 px-5 flex justify-center">
 	<Link href="https://github.com/cuvar/panal">GitHub</Link>
-</div>
+</footer>
