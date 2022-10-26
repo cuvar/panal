2129088f0dc13e4cec52c6c417fc1cf3d43bbb2d
cuvar <luca-mueller@gmx.net>
2022-10-26 17:43:46 +0200

    feat: initial react commit

--- src/components/LinkWidget/LinkContainer.svelte
+++
@@ -1,3 +0,0 @@
-<div class="flex flex-col justify-center w-20 text-ellipsis whitespace-nowrap overflow-hidden">
-	<slot />
-</div>
--- src/components/Navbar.svelte
+++
@@ -1,3 +0,0 @@
-<nav class="w-full bg-panal-700 py-5 px-5">
-	<div>panal</div>
-</nav>
