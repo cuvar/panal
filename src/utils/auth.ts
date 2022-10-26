1899f8b1c23b8feed17776b80b64dc44b41e33f6
cuvar <luca-mueller@gmx.net>
2022-10-26 22:56:12 +0200

    fix: ts error in auth file

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,9 @@
 function getTokenFromCookie(cookieName: string): string {
-  const cookies = document.cookie.split(";");
+  if (typeof document === "undefined") return "";
+
+  const cookies = document.cookie.split(";") ?? [];
   for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
+    const cookie = cookies[i]!.trim();
     if (cookie.startsWith(`${cookieName}=`)) {
       return cookie.substring(cookieName.length + 1);
     }
