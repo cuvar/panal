013005265addf753249e481f1c0a8342bae978e7
cuvar <luca-mueller@gmx.net>
2022-11-05 17:03:30 +0100

    feat: basic grid for widgets

--- src/components/WidgetView.tsx
+++ src/components/WidgetView.tsx
@@ -2,6 +2,7 @@ import SearchWidget from "./SearchWidget";
 import TimeWidget from "./TimeWidget";
 import CalendarWidget from "./CalendarWidget";
 import LinkCollectionWidget from "./LinkWidget/LinkCollectionWidget";
+import styles from "./WidgetView.module.css";
 
 interface IProps {
   data: WidgetViewData;
@@ -10,10 +11,35 @@ interface IProps {
 export default function WidgetView(props: IProps) {
   return (
     <>
-      <TimeWidget />
-      <SearchWidget />
-      <LinkCollectionWidget />
-      <CalendarWidget calendarData={props.data.calendarData} />
+      <div
+        className={`h-full grid ${styles["auto-rows"]} ${styles["sm-cols-3"]} ${styles["md-cols-6"]} ${styles["xl-cols-10"]} gap-4 sm:gap-6 bg-green-500`}
+      >
+        <div className=" bg-blue-500">Hello</div>
+        <div className="bg-yellow-500">Hello</div>
+        <div className="bg-red-500 col-span-3 row-span-3">Hello</div>
+        {/* <div className=" bg-blue-500">Hello</div>
+        <div className="bg-yellow-500">Hello</div>
+        <div className="bg-red-500">Hello</div>
+        <div className=" bg-blue-500">Hello</div>
+        <div className="bg-yellow-500">Hello</div>
+        <div className="bg-red-500">Hello</div>
+        <div className=" bg-blue-500">Hello</div>
+        <div className="bg-yellow-500">Hello</div>
+        <div className="bg-red-500">Hello</div> */}
+      </div>
     </>
   );
 }
+
+// grid-cols-3 md:grid-cols-6 xl:grid-cols-10
+
+// <div>
+//   {/* w200xh100 */}
+//   <TimeWidget />
+//   {/* w300xh100 */}
+//   <SearchWidget />
+//   {/* w400xh400 */}
+//   <LinkCollectionWidget />
+//   {/* w300xh300 */}
+//   <CalendarWidget calendarData={props.data.calendarData} />
+// </div>
