b2a3b484bdad52b3daa227b05f0b8317348ce4f1
cuvar <luca-mueller@gmx.net>
2023-10-01 11:57:55 +0200

    refactor: min_w and min_h as const

--- src/server/service/computeSizeForWidgetService.ts
+++ src/server/service/computeSizeForWidgetService.ts
@@ -1,32 +1,11 @@
+import { MIN_WIDGET_HEIGHT, MIN_WIDGET_WIDTH } from "~/utils/const";
 import type { WidgetType } from "~/utils/types/widget";
 
 // todo: write tests
 export function getMinWidthForWidget(widgetType: WidgetType): number {
-  switch (widgetType) {
-    case "time":
-      return 2;
-    case "search":
-      return 2;
-    case "links":
-      return 4;
-    case "calendar":
-      return 2;
-    default:
-      return 1;
-  }
+  return MIN_WIDGET_WIDTH[widgetType] ?? MIN_WIDGET_WIDTH.default;
 }
 
 export function getMinHeightForWidget(widgetType: WidgetType): number {
-  switch (widgetType) {
-    case "time":
-      return 1;
-    case "search":
-      return 1;
-    case "links":
-      return 1;
-    case "calendar":
-      return 2;
-    default:
-      return 1;
-  }
+  return MIN_WIDGET_HEIGHT[widgetType] ?? MIN_WIDGET_HEIGHT.default;
 }
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -8,3 +8,19 @@ export const BREAKPOINTS: Readonly<Record<ScreenSize, number>> = {
 };
 
 export const BREAKPOINTS_ORDER = Object.keys(BREAKPOINTS) as ScreenSize[];
+
+export const MIN_WIDGET_WIDTH = Object.freeze({
+  time: 2,
+  search: 2,
+  links: 3,
+  calendar: 2,
+  default: 1,
+});
+
+export const MIN_WIDGET_HEIGHT = Object.freeze({
+  time: 1,
+  search: 1,
+  links: 1,
+  calendar: 1,
+  default: 1,
+});
