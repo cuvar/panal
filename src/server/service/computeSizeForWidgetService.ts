2cf636c006f05c5662a38c482dd444388fedbeeb
cuvar <luca-mueller@gmx.net>
2023-10-08 10:52:11 +0200

    test: computSizeForWidgetService

--- src/server/service/computeSizeForWidgetService.ts
+++ src/server/service/computeSizeForWidgetService.ts
@@ -1,7 +1,6 @@
 import { MIN_WIDGET_HEIGHT, MIN_WIDGET_WIDTH } from "~/utils/const";
 import type { WidgetType } from "~/utils/types/widget";
 
-// todo: write tests
 export function getMinWidthForWidget(widgetType: WidgetType): number {
   return MIN_WIDGET_WIDTH[widgetType] ?? MIN_WIDGET_WIDTH.default;
 }
