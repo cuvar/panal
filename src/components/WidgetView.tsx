f002925dd969ce1b6200c3d103ba84bbea01e0d7
cuvar <luca-mueller@gmx.net>
2023-10-31 19:50:33 +0100

    fix: deprecated verticalCompact replaced

--- src/components/WidgetView.tsx
+++ src/components/WidgetView.tsx
@@ -70,7 +70,7 @@ export default function WidgetView(props: Props) {
         rowHeight={GRID_ROW_HEIGHT}
         layouts={widgetLayout}
         maxRows={GRID_MAX_ROW}
-        verticalCompact={false}
+        compactType={null}
         autoSize={false}
         onLayoutChange={handleLayoutChange}
       >
