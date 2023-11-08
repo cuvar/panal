10af70929bb6b1a9b26a485de131b2194843dcb9
cuvar <luca-mueller@gmx.net>
2023-11-08 20:07:37 +0100

    feat: rework link widget

--- src/server/widgets/links/LinkWidget/LinkWidget.tsx
+++ src/server/widgets/links/LinkWidget/LinkWidget.tsx
@@ -6,22 +6,6 @@ type Props = {
   tab?: "new" | "same";
 };
 
-// ! this needs a web scraper from the backend
-// function getFavicon() {
-// 	const linkTags = document.querySelectorAll("[rel='icon']");
-// 	const faviconRefs = linkTags.forEach((linkTag) => {
-// 		if (linkTag.getAttribute('href') !== null) {
-// 			return linkTag.getAttribute('href');
-// 		}
-// 	});
-// <!-- todo: look in brave dev tools for how they do it
-
-// 	console.log(faviconRefs);
-// }
-
-const LINK_CLASS =
-  "bg-teal-200 hover:bg-teal-300 text-black p-5 rounded-md w-full h-20 flex justify-center items-center";
-
 export default function LinkWidget(props: Props) {
   return (
     <>
@@ -29,11 +13,13 @@ export default function LinkWidget(props: Props) {
         href={props.href}
         target={props.tab === "same" ? "_self" : "_blank"}
         rel={props.tab === "same" ? "" : "noopener noreferrer"}
-        className={LINK_CLASS}
+        className="flex h-14 w-14 items-center justify-center rounded-md bg-teal-200 p-5 text-black hover:bg-teal-300"
       >
         {linkIcon}
       </a>
-      <span className="w-full text-center text-sm">{props.text}</span>
+      <span className="h-10 w-full whitespace-pre-wrap text-center text-sm">
+        {props.text}
+      </span>
     </>
   );
 }
