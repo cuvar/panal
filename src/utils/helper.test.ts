6a384986f88f37d4b9df43a7b0512b456a1495bc
cuvar <luca@cuvar.dev>
2023-11-29 19:53:47 +0100

    test: findFreeSpace and overlaps

--- src/utils/helper.test.ts
+++ src/utils/helper.test.ts
@@ -2,7 +2,12 @@
  * @jest-environment node
  */
 
-import { generateUniqueID, isEmptyPositioning, isSameSet } from "./helper";
+import {
+  generateUniqueID,
+  isEmptyPositioning,
+  isSameSet,
+  overlaps,
+} from "./helper";
 
 describe("helper functions", () => {
   it("generateUniqueID works correctly", () => {
@@ -57,6 +62,46 @@ describe("helper functions", () => {
     };
     // act
     const result = isEmptyPositioning(input);
+    // assert
+    expect(result).toBe(false);
+  });
+
+  it("overlaps works correctly when widgets do overlap", () => {
+    // arrange
+    const w1 = {
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 2,
+    };
+    const w2 = {
+      x: 1,
+      y: 1,
+      w: 2,
+      h: 2,
+    };
+    // act
+    const result = overlaps(w1, w2);
+    // assert
+    expect(result).toBe(true);
+  });
+
+  it("overlaps works correctly when widgets do not overlap", () => {
+    // arrange
+    const w1 = {
+      x: 0,
+      y: 0,
+      w: 1,
+      h: 1,
+    };
+    const w2 = {
+      x: 2,
+      y: 2,
+      w: 1,
+      h: 1,
+    };
+    // act
+    const result = overlaps(w1, w2);
     // assert
     expect(result).toBe(false);
   });
