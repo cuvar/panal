f13d000b51c617d1dd283a0d5dd29ebc8cd62045
cuvar <luca@cuvar.dev>
2023-12-03 13:58:43 +0100

    feat: very simple landingpage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import type { Session } from "next-auth";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
+import GhostButton from "./Button/GhostButton";
 import Menu from "./Menu";
 
 type Props = {
@@ -7,9 +9,18 @@ type Props = {
 };
 
 export default function Navbar(props: Props) {
+  function login() {
+    void signIn();
+  }
+
   return (
     <nav className="flex h-20 w-full items-center justify-between bg-panal-700 px-5 py-5">
       <Link href="/">panal</Link>
+      {!props.sesh && (
+        <GhostButton className="px-4 py-2" onClick={login}>
+          Sign in
+        </GhostButton>
+      )}
       {props.sesh ? <Menu /> : null}
     </nav>
   );
--- src/sites/Landing.tsx
+++ src/sites/Landing.tsx
@@ -1,34 +1,44 @@
 import Head from "next/head";
-import Navbar from "~/components/Navbar";
 import Footer from "~/components/Footer";
-
-import { signIn } from "next-auth/react";
+import Link from "~/components/Link";
+import Navbar from "~/components/Navbar";
 
 export default function Login() {
-  function login() {
-    void (async () => {
-      await signIn();
-    })();
-  }
-
   return (
     <>
       <Head>
         <title>panal</title>
+        <meta
+          name="description"
+          content="a simple dashboard for more productivity"
+        />
       </Head>
       <div className="flex h-full min-h-screen flex-col justify-between text-gray-100 ">
         <Navbar sesh={null} />
         <main className="flex h-full flex-col items-center bg-panal-500 px-5">
-          <div className="flex h-screen w-screen items-center justify-center">
-            <div className="flex flex-col space-y-4">
-              <h1 className="text-xl font-bold">Not signed in</h1>
-              <button
-                className="hover:underline active:text-blue-500"
-                onClick={login}
-              >
-                Sign in
-              </button>
+          <div className="flex h-screen w-screen flex-col items-center justify-center bg-panal-700 sm:flex-row">
+            {/* left */}
+            <div className="flex h-full w-full flex-col justify-center space-y-10 px-10 ">
+              <h1 className="text-3xl font-semibold sm:text-4xl md:text-5xl">
+                A simple dashboard for
+                <br />
+                more productivity
+              </h1>
+              <p className="italic">
+                This project is still under development. More information can be
+                found{" "}
+                <Link
+                  href={"https://github.com/cuvar/panal"}
+                  tab="new"
+                  className="underline"
+                >
+                  here
+                </Link>
+                .
+              </p>
             </div>
+            {/* right */}
+            <div className="flex h-full w-full flex-col justify-center"></div>
           </div>
         </main>
         <Footer />
