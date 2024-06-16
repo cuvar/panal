2c1308dec5263a083216ba51c3f9e103be42029f
cuvar <luca@cuvar.dev>
2024-06-16 13:35:19 +0200

    refactor: comment in store

--- web/src/lib/ui/store.ts
+++ web/src/lib/ui/store.ts
@@ -2,8 +2,8 @@ import { atom } from "jotai";
 import type GridLayout from "react-grid-layout";
 import type { ToastType } from "../types/types";
 
-export const toastTextAtom = atom("");
-export const toastTypeAtom = atom<ToastType>("info");
-export const editModeAtom = atom(false);
-export const widgetLayoutAtom = atom<GridLayout.Layouts>({});
-export const editedWidgetLayoutAtom = atom<GridLayout.Layouts>({});
+export const toastTextAtom = atom(""); // the text to display in the toast
+export const toastTypeAtom = atom<ToastType>("info"); // the type of toast to display
+export const editModeAtom = atom(false); // whether the user is in edit mode
+export const widgetLayoutAtom = atom<GridLayout.Layouts>({}); // the layout of the widgets
+export const editedWidgetLayoutAtom = atom<GridLayout.Layouts>({}); // the layout that is currently being edited
