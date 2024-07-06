c4bf4afd6ac096c90073a9aea794991c6e9c0d58
cuvar <luca@cuvar.dev>
2024-07-06 12:45:08 +0200

    fix: new codes for layout repo

--- web/src/server/domain/layout/repo/layoutLocalFileRepository.ts
+++ web/src/server/domain/layout/repo/layoutLocalFileRepository.ts
@@ -35,23 +35,33 @@ export class LayoutLocalFileRepository implements LayoutRepository {
 
   async getAll(): Promise<AdjustedWidgetLayout[]> {
     try {
-      const fileContents = await this.reader.read(this.file);
-      const response = JSON.parse(fileContents);
-
-      if (!response) {
+      const content = await this._parseFileContent();
+      if (!content) {
         throw new AppError(codes.WIDGET_NONE_FOUND);
       }
-      if (typeof response !== "object") {
+      if (typeof content !== "object") {
         throw new AppError(codes.REPOSITORY_INVALID_RESPONSE);
       }
-      const config = parseAdjustedWidgetLayout(JSON.stringify(response));
+      const config = parseAdjustedWidgetLayout(JSON.stringify(content));
       if (!config) {
         throw new AppError(codes.WIDGET_CONFIG_INVALID);
       }
-
       return config;
     } catch (error) {
       throw new AppError(codes.REPOSITORY_GET_ALL_LAYOUT_FAILED, error);
+    }
+  }
+
+  async _parseFileContent() {
+    try {
+      const fileContents = await this.reader.read(this.file);
+      if (!fileContents) {
+        throw new AppError(codes.REPOSITORY_EMPTY_CONTENT);
+      }
+      const content = JSON.parse(fileContents);
+      return content;
+    } catch (error) {
+      throw new AppError(codes.REPOSITORY_PARSE_FILE_CONTENT_FAILED, error);
     }
   }
 
